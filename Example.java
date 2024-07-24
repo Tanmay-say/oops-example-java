4e626d46cb7edd3d5b92390cf48ff9f523562d83
tanmaysayare <tanmaysayare12345@gmail.com>
2024-07-24 10:49:14 +0530

    private key word

--- Example.java
+++ Example.java
@@ -1,6 +1,7 @@
 public class Example {
     public static void main(String[] args) {
-        Students S1 = new Students("Tanmay", 20);
+        Students S1 = new Students();
+        S1.students("Tanmay", 20);
         S1.display();
     }
 }
--- Students.java
+++ Students.java
@@ -1,11 +1,24 @@
 
 class Students {
-    String name;
-    int age;
-    Students(String name, int age) {
-        this.name = name;
-        this.age = age;
+    private String name;
+    private int age;
+    public void setAge(int age){
+        if(age < 0) {
+            System.out.println("Age cannot be negative");
+            System.exit(0);
+        }
+        else{
+            this.age = age;
+        }
     }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void students(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+        
     void display() {
         System.out.println("Name: " + name + " Age: " + age);
     }
