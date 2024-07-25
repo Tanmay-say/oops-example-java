6fff9c224c08f8012b625590c608c20cd40c408a
tanmaysayare <tanmaysayare12345@gmail.com>
2024-07-25 12:48:16 +0530

    Merge Two Sorted LinkedList

--- Linkedlistoperations/MergeLinkedList.java
+++ Linkedlistoperations/MergeLinkedList.java
@@ -1,5 +1,4 @@
 package Linkedlistoperations;
-
 class ListNode {
     int val;
     ListNode next;
@@ -9,16 +8,11 @@ class ListNode {
 }
 
 public class MergeLinkedList {
-
-    // Method to merge two sorted linked lists
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // Create a dummy node to start the merged list
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
 
-        // While both lists are not empty
         while (l1 != null && l2 != null) {
-            // Compare the values and attach the smaller node to the merged list
             if (l1.val <= l2.val) {
                 current.next = l1;
                 l1 = l1.next;
@@ -29,14 +23,12 @@ public class MergeLinkedList {
             current = current.next;
         }
 
-        // Attach the remaining part of the list, if any
         if (l1 != null) {
             current.next = l1;
         } else {
             current.next = l2;
         }
 
-        // Return the merged list, which starts at dummy.next
         return dummy.next;
     }
     public static void print(ListNode current){
@@ -47,13 +39,13 @@ public class MergeLinkedList {
     }
 
     public static void main(String[] args) {
-        // Creating the first sorted linked list: 1 -> 3 -> 5
+
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(3);
         l1.next.next = new ListNode(5);
         System.out.println("First List");
         print(l1);
-        // Creating the second sorted linked list: 2 -> 4 -> 6
+
         ListNode l2 = new ListNode(2);
         l2.next = new ListNode(4);
         l2.next.next = new ListNode(6);
