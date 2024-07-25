fbcda9fb312ff3537c3eb4d01dcd491eced165bb
tanmaysayare <tanmaysayare12345@gmail.com>
2024-07-25 11:49:06 +0530

    LinkedList has cycle or not

---
+++ Linkedlistoperations/Circlular.java
@@ -0,0 +1,32 @@
+package Linkedlistoperations;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class Circlular {
+    public static boolean hasCycle(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next!= null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                return true;
+            }
+        }        
+        return false;
+        }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        boolean hasCycle = hasCycle(head);
+        System.out.println("The linked list has a cycle: " + hasCycle);
+    }
+    
+}
