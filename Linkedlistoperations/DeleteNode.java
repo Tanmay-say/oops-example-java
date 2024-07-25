497790b1945dc69b1f18ee1ddbe077f7ee49da5d
tanmaysayare <tanmaysayare12345@gmail.com>
2024-07-25 12:20:47 +0530

    Delete from singly linked list

---
+++ Linkedlistoperations/DeleteNode.java
@@ -0,0 +1,35 @@
+package Linkedlistoperations;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class DeleteNode {
+    public static void delete(ListNode node) {
+        if (node == null || node.next == null) {
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        ListNode nodeToDelete = head.next; // Node with value 5
+        delete(nodeToDelete);
+
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+    }
+}
