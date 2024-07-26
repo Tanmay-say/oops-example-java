170047f788b9167ab38a4b977692ca872036a274
tanmaysayare <tanmaysayare12345@gmail.com>
2024-07-26 12:18:39 +0530

    Tree updated

--- trees/Inorder.java
+++ trees/Inorder.java
@@ -26,18 +26,18 @@ public class Inorder {
         // Creating a tree from given post-order: 8 10 9 14 2 12 3 6 11 5 15 4 1
 
         TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(4);
-        root.right = new TreeNode(15);
-        root.left.left = new TreeNode(5);
-        root.left.left.left = new TreeNode(11);
-        root.left.left.left.left = new TreeNode(6);
-        root.left.left.left.right = new TreeNode(3);
-        root.left.left.left.right.right = new TreeNode(12);
-        root.left.left.left.right.right.left = new TreeNode(2);
-        root.left.left.left.right.right.left.right = new TreeNode(14);
-        root.left.left.left.right.right.left.left = new TreeNode(9);
-        root.left.left.left.right.right.left.left.right = new TreeNode(10);
-        root.left.left.left.right.right.left.left.left = new TreeNode(8);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(8);
+        root.left.right = new TreeNode(14);
+        root.left.right.left = new TreeNode(9);
+        root.left.right.left.left = new TreeNode(10);
+        root.right.right = new TreeNode(15);
+        root.right.right.left = new TreeNode(5);
+        root.right.right.left.right = new TreeNode(11);
+        root.right.right.left.right.left = new TreeNode(3);
+        root.right.right.left.right.left.left = new TreeNode(12);
+        root.right.right.left.right.right = new TreeNode(6);
 
         List<Integer> result = new ArrayList<>();
         ans(root, result);
